3e97b182b6871f7d1ed4754cf1d89ed21e56d433
Rishabh Gupta <rishotics@gmail.com>
2022-11-02 01:32:03 +0530

    add util

--- src/pages/SetupWallet.jsx
+++ src/pages/SetupWallet.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import qrImg from "../assets/image/qr.png"
+import {generateMerkleTree} from "./util"
+
+
 
 const SetupWallet = () => {
+
+  const deploy = async() => {
+    const {uri, secret, root} = await generateMerkleTree()
+    console.log(`uri:`)
+  }
+  // await deploy()
   return (
     <div className="d-flex row justify-content-center m-4">
       <h3>Wallet Setup</h3>
