bf59e2b6973c439d7862e9515041cf82452f2691
Rishabh Gupta <rishotics@gmail.com>
2022-11-10 01:13:09 +0530

    add ui

---
+++ ui/src/dist/types/index.js
@@ -0,0 +1,29 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+exports.__esModule = true;
+exports.Verifier__factory = exports.OTPFactory__factory = exports.OTP__factory = exports.MyWalletDeployer__factory = exports.MyWallet__factory = exports.IVerifier__factory = exports.Greeter__factory = exports.factories = void 0;
+exports.factories = require("./factories");
+var Greeter__factory_1 = require("./factories/Greeter__factory");
+__createBinding(exports, Greeter__factory_1, "Greeter__factory");
+var IVerifier__factory_1 = require("./factories/IVerifier__factory");
+__createBinding(exports, IVerifier__factory_1, "IVerifier__factory");
+var MyWallet__factory_1 = require("./factories/MyWallet__factory");
+__createBinding(exports, MyWallet__factory_1, "MyWallet__factory");
+var MyWalletDeployer__factory_1 = require("./factories/MyWalletDeployer__factory");
+__createBinding(exports, MyWalletDeployer__factory_1, "MyWalletDeployer__factory");
+var OTP__factory_1 = require("./factories/OTP__factory");
+__createBinding(exports, OTP__factory_1, "OTP__factory");
+var OTPFactory__factory_1 = require("./factories/OTPFactory__factory");
+__createBinding(exports, OTPFactory__factory_1, "OTPFactory__factory");
+var Verifier__factory_1 = require("./factories/Verifier__factory");
+__createBinding(exports, Verifier__factory_1, "Verifier__factory");
